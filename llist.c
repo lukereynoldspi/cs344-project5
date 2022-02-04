3355df6d72780201b0537f5436634e8b79448b51
Luke Reynolds <lukereynoldspi@gmail.com>
2022-02-04 14:43:00 -0800

    updated method parameters

--- llist.c
+++ llist.c
@@ -9,61 +9,69 @@ int main(int argc, char *argv[])
 	if (argc < 2){
 		perror("Not enough arguments");
 	}
+	
+
 	else {
 		for (int i = 0; i < argc; ++i) {
 			if (strcmp(argv[i],"ih") == 0) {
-				printf("%s\n", "ih" ); 
 				char *s = argv[i + 1];
 				int num = atoi(s);
-				printf("%d\n", num);
+
+				struct node *head = NULL;
+    			struct node *n = node_alloc(10);
+    			llist_insert_head(&head, n);
 			}
 			else if (strcmp(argv[i],"it") == 0) {
-				printf("%s\n", "it" );
 				char *s = argv[i + 1];
-				int num = atoi(s);					
-				printf("%d\n", num);
+				int num = atoi(s);
+				struct node *head = NULL;
+    			struct node *n = node_alloc(10);
+				llist_insert_tail(&head, n);
 			}
 
 			else if (strcmp(argv[i],"dh") == 0) {
-				//llist_delete_head();
+				llist_delete_head();
 				printf("%s\n", "dh" );
 			}
 
 			else if (strcmp(argv[i],"f") == 0) {
-				//llist_free();
+				llist_free();
 				printf("%s\n", "f" );
 			}
 
 			else if (strcmp(argv[i],"p") == 0) {
-				//llist_print();
+				llist_print();
 				printf("%s\n", "p" );
 			}
 		}
 	}
 }
 
-/*
-int llist_insert_head(){
 
-}
-int llist_insert_tail(){
-
-}
-void llist_delete_head(){
-
-}
-void llist_free(){
-
-}
-void llist_print(){
+void llist_insert_head(struct node **head, struct node *n) {
 
 }
 
-int node_alloc(){
+void llist_insert_tail(struct node **head, struct node *n) {
 
 }
 
-void node_free(){
+struct node *llist_delete_head(struct node **head) {
 
 }
-*/
+
+void llist_print(struct node *head) {
+
+}
+
+void llist_free(struct node **head){
+
+}
+
+struct node *node_alloc(int value) {
+
+}
+
+void node_free(struct node *n) {
+
+}
